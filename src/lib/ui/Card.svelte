dfee3edfc6d3bde5be26c51c7ab42f1ec80a13f7
Marty Rath <martydavidger@gmail.com>
2024-05-04 15:49:47 +0100

    Added report subtitle

--- src/lib/ui/Card.svelte
+++ src/lib/ui/Card.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    export let title = "";
-  </script>
-  
-  <div class="card mb-5">
-    <header class="card-header">
-      <p class="card-header-title">
-        <span class="icon"><i class="fas fa-tree"></i></span><span>{title}</span>
-      </p>
-    </header>
-    <div class="card-content">
-      <div class="content">
-        <slot />
-      </div>
+  export let title = "";
+</script>
+
+<div class="card mb-5">
+  <header class="card-header">
+    <p class="card-header-title">
+      <span class="icon"><i class="fas fa-tree"></i></span><span>{title}</span>
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <slot />
     </div>
   </div>
-  
+</div>
--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import RecentlyAddedTrees from "$lib/ui/RecentlyAddedTrees.svelte";
-    import Card from "$lib/ui/Card.svelte";
-  </script>
-  
+  import RecentlyAddedTrees from "$lib/ui/RecentlyAddedTrees.svelte";
+  import Card from "$lib/ui/Card.svelte";
+  import { subTitle } from "$lib/stores";
+
+  subTitle.set("View trees recently added by our contributors");
+</script>
+
 <Card title="Recently Added Trees">
-<RecentlyAddedTrees />
+  <RecentlyAddedTrees />
 </Card>
