6a33e41d2c9c5301d00b1bd1a4fff1fa29c1216b
Marty Rath <martydavidger@gmail.com>
2024-05-07 20:36:40 +0100

    signup working, no first, last name

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,9 +1,29 @@
 // place files you want to import through the `$lib` alias in this folder.
-
 // Values that can be changed from anywhere in an application, and updated dynamically on any UX they are embedded in
+
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { writable } from "svelte/store";
+import { auth } from "./firebase/firebase";
 
 // Subtitle to be used on a window
 export const subTitle = writable<string>();
 // Email of currently logged in user
 export const currentSession = writable<string>();
+
+export const authStore = writable({
+    user: null,
+    loading: true,
+    data: {}
+})
+
+export const authHandlers = {
+    signup: async (email, password) => {
+        await createUserWithEmailAndPassword(auth, email, password)
+    },
+    login: async (email, password) => {
+        await signInWithEmailAndPassword(auth, email, password)
+    },
+    logout: async () => {
+        await signOut(auth)
+    }
+}
--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -3,18 +3,29 @@
   import { currentSession } from "$lib/stores";
   import Message from "$lib/ui/Message.svelte";
   import UserCredentials from "$lib/ui/UserCredentials.svelte";
+  import { authHandlers } from "$lib/stores";
 
   let email = "";
   let password = "";
   let message = "";
+  let authenticating = false;
 
-  function login(){
+  async function login(){
+    authenticating = true;
     if (!email || !password) {
     message = "Wrong credentials entered";
     return;
     }
-  }
 
+  try {
+      await authHandlers.login(email, password);
+    } catch (err) {
+      console.log("There was an auth error", err);
+      message = "Authentication failed";
+    } finally {
+      authenticating = false;
+    }
+  }
 </script>
   
   {#if message}
@@ -23,6 +34,8 @@
   <!-- This is an HTML comment  Triggers login function above -->
   <form on:submit|preventDefault={login}>
     <UserCredentials bind:email bind:password />
-    <button class="button is-success is-fullwidth">Log In</button>
+    <button class="button is-success is-fullwidth">
+      Log In
+    </button>
   </form>
   
