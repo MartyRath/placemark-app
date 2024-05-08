095d04d5fb3f7d8dafbbd1f8d01107fe3785922b
Marty Rath <martydavidger@gmail.com>
2024-05-08 11:03:49 +0100

    Updated store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -7,3 +7,21 @@ import { auth } from "./firebase/firebase";
 
 // Subtitle to be used on a window
 export const subTitle = writable<string>();
+
+export const authStore = writable({
+    user: null,
+    loading: true,
+    data: {}
+  });
+  
+  export const authHandlers = {
+    signup: async (email, password) => {
+      await createUserWithEmailAndPassword(auth, email, password);
+    },
+    login: async (email, password) => {
+      await signInWithEmailAndPassword(auth, email, password);
+    },
+    logout: async () => {
+      await signOut(auth);
+    }
+  };
