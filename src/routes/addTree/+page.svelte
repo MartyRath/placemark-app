6581b2153bf77f9fd6fb8df8fc68655caae9f55b
Marty Rath <martydavidger@gmail.com>
2024-05-09 19:16:31 +0100

    Card title dynamically updates based on editingMode

--- src/routes/addTree/+page.svelte
+++ src/routes/addTree/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import Card from "$lib/ui/Card.svelte";
-  import { subTitle } from "$lib/stores";
+  import { subTitle, editingMode } from "$lib/stores";
   import AddTreeForm from "./AddTreeForm.svelte";
 
   subTitle.set("Add a Tree");
+  let cardTitle = "";
+
+  // Dynamically update card title based on editingMode
+  $: {
+    // Update the title based on the value of editingMode
+    if ($editingMode) {
+      cardTitle = "Edit your tree";
+    }
+    else {
+      cardTitle = "Add your tree";
+    }
+  }
 </script>
 
-<Card title="Found a tree worth noting? Enter details below">
+<Card title={cardTitle}>
   <AddTreeForm />
 </Card>
