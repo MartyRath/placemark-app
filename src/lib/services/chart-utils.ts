76295e669bffd16f50ab4d3890eb691ebb5739b9
Marty Rath <martydavidger@gmail.com>
2024-05-10 13:48:10 +0100

    Added pie chart logic to chart utils

--- src/lib/services/chart-utils.ts
+++ src/lib/services/chart-utils.ts
@@ -3,11 +3,7 @@ import type { DataSet } from "$lib/types/placemark-types";
 export function generateUserTreeByHeight(userTreesList: any[]): DataSet {
   const userTreeByHeight: DataSet = {
     labels: [],
-    datasets: [
-      {
-        values: []
-      }
-    ]
+    datasets: [{ values: [] }]
   };
 
   userTreesList.forEach((tree) => {
@@ -17,3 +13,38 @@ export function generateUserTreeByHeight(userTreesList: any[]): DataSet {
 
   return userTreeByHeight;
 }
+
+export function generateUserTreeSpeciesDistribution(userTreesList: any[]): DataSet {
+  const userTreeBySpecies: DataSet = {
+    labels: [],
+    datasets: [{ values: [] }]
+  };
+
+  // Returns list of tree species
+  const speciesList = userTreesList.map((tree) => tree.species);
+  // Counts how many times a species occurs
+  const speciesCount = countSpecies(speciesList);
+  
+  // Add key values to labels (species, no duplicates)
+  userTreeBySpecies.labels = Array.from(speciesCount.keys());
+  // Add count of species to datasets
+  userTreeBySpecies.datasets[0].values = Array.from(speciesCount.values());
+
+  return userTreeBySpecies;
+}
+
+  // This function counts the frequency of each species from the user's trees.
+  function countSpecies(speciesList: string[]): Map<string, number> {
+    const speciesCount = new Map<string, number>();
+    // Iterating through each species in list
+    speciesList.forEach((species) => {
+      // Checks if species is already mapped
+      if (speciesCount.has(species)) {
+        speciesCount.set(species, speciesCount.get(species)! + 1);
+      } else {
+        // If species already exists, adds to count
+        speciesCount.set(species, 1);
+      }
+    });
+    return speciesCount;
+  }
--- src/routes/charts/Charts.svelte
+++ src/routes/charts/Charts.svelte
@@ -3,11 +3,9 @@
   import Chart from "svelte-frappe-charts";
   import Card from "$lib/ui/Card.svelte";
   import { authStore } from "$lib/stores";
-  import type { DataSet } from "$lib/types/placemark-types";
-  import { generateUserTreeByHeight } from "$lib/services/chart-utils";
+  import { generateUserTreeByHeight, generateUserTreeSpeciesDistribution } from "$lib/services/chart-utils";
 
   let userTreesList: any[] = [];
-  let speciesList: any[] = [];
   let barChartData: any = {};
   let pieChartData: any = {};
 
@@ -20,37 +18,13 @@
     }
   });
 
+  // Pushes populated userTreesList to charts
   function updateChartData() {
-    // Shows height of each user tree
     barChartData = generateUserTreeByHeight(userTreesList);
-
-    // Returns list of tree species
-    speciesList = userTreesList.map((tree) => tree.species);
-    // Counts how many times a species occurs
-    const speciesCount = countSpecies(speciesList);
-    // Pie chart showing species distribution
-    pieChartData = {
-      labels: Array.from(speciesCount.keys()), // Species names as labels
-      datasets: [{ values: Array.from(speciesCount.values()) }]
-    };
-  }
-
-  // This function counts the frequency of each species from the user's trees.
-  function countSpecies(speciesList: string[]): Map<string, number> {
-    const speciesCount = new Map<string, number>();
-    // Iterating through each species in list
-    speciesList.forEach((species) => {
-      // Checks if species is already mapped
-      if (speciesCount.has(species)) {
-        speciesCount.set(species, speciesCount.get(species)! + 1);
-      } else {
-        // If species already exists, adds to count
-        speciesCount.set(species, 1);
-      }
-    });
-    return speciesCount;
+    pieChartData = generateUserTreeSpeciesDistribution(userTreesList);
   }
 </script>
+
 
 {#if !$authStore.loading}
   <div class="columns">
