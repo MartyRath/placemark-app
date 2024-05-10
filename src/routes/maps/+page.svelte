5141f018179050f1ba3dd13a286160c9a6cc8518
Marty Rath <martydavidger@gmail.com>
2024-05-10 19:32:24 +0100

    "window is not defined" error resolved by using "leaflet" instead of importing "L" for baseLayers

--- src/routes/maps/+page.svelte
+++ src/routes/maps/+page.svelte
@@ -3,6 +3,9 @@
   import Heading from "$lib/ui/Heading.svelte";
   import Card from "$lib/ui/Card.svelte";
   import LeafletMap from "$lib/ui/LeafletMap.svelte";
+  import { subTitle } from "$lib/stores";
+
+subTitle.set("Find your trees");
 </script>
 
 <Card title="Tree Locations">
