961d048785687e06445b361f523a9acc86de8406
Marty Rath <martydavidger@gmail.com>
2024-05-18 11:33:36 +0100

    fixed logout

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
   import { authHandlers } from "$lib/stores";
 
-  const handleLogout = async () => {
+  export const handleLogout = async () => {
     await authHandlers.logout();
-    goto("/");
+    location.href = "/";
   };
 
   handleLogout();
